795b2e7a3de3d08a96b3b0759dfaa36337a66969
mokchenda <mokchend@yahoo.com>
2018-09-13 23:41:28 +0200

    HTTPS package

--- server/main.js
+++ server/main.js
@@ -1,4 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+// somewhere within your server code
+
+if (Meteor.isServer) {
+SSL('/home/chenda/Dropbox/code/nodejs/meteor/jsforcemeteor/private/localhost.key','/home/chenda/Dropbox/code/nodejs/meteor/jsforcemeteor/private/localhost.cert', 3443);
+}
 
 Meteor.startup(() => {
   // code to run on server at startup
