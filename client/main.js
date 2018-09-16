507c407543a481fe19b3e46a681c8273f354e143
mokchenda <mokchend@yahoo.com>
2018-09-16 19:48:03 +0200

    31 - FInished player component

--- client/main.js
+++ client/main.js
@@ -5,20 +5,15 @@ import { Players } from './../imports/api/players.js';
 import { Tracker } from 'meteor/tracker';
 import TitleBar from '../imports/ui/TitleBar.js';
 import AddPlayer from '../imports/ui/AddPlayer.js';
+import Player from '../imports/ui/Player.js';
 //import { greetUser, writeFileSync } from '../imports/helpers/fileHelper.js';
 var fileHelper = require('./../imports/helpers/fileHelper.js');
 
 const renderPlayers =  (playersList) => {
   return playersList.map( (player) => {
     //parenthesis allow jsx on multiple line for better formatting
-    return (
-    <p key={player._id}>
-      {player.name} has {player.score} point(s).
-      <button onClick={() => Players.update(player._id, { $inc: { score: -1 } })}>-1</button>
-      <button onClick={() => Players.update(player._id, { $inc: { score: 1 } })}>+1</button>
-      <button onClick={() => Players.remove(player._id)}>X</button>
-    </p>
-    );
+    //WARNING: Each child in an array or iterator should have a unique "key" prop.
+    return <Player key={player._id} player={player}/>;
   });
 };
 
