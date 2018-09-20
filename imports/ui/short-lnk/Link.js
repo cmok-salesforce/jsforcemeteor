b2225445c204359959c2a53a645423343a574737
mokchenda <mokchend@yahoo.com>
2018-09-20 06:35:41 +0200

    56-Component state inline sample

--- imports/ui/short-lnk/Link.js
+++ imports/ui/short-lnk/Link.js
@@ -1,17 +1,5 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import {  browserHistory } from 'react-router-dom';
-//import { browserHistory } from 'react-router';
-import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types'; // ES6
-
-
-import { createBrowserHistory } from 'history';
-const browserHistory = createBrowserHistory();
-
-
-//https://github.com/ReactTraining/react-router/blob/master/FAQ.md#how-do-i-access-the-history-object-outside-of-components
 
 export default class Link extends React.Component{
     onLogout(event) {
--- imports/ui/short-lnk/Signup.js
+++ imports/ui/short-lnk/Signup.js
@@ -4,11 +4,19 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 export default class Signup extends React.Component{
+    constructor(props) {
+     super(props);
+     this.state = {
+        count: 0
+     };
+    }
     render() {
         return (
             <div>
                 <h1>Join Short Link</h1>
                 Signup Form Component
+               <p> {this.state.count} </p>
+               <button onClick={()=>{this.setState({count: this.state.count +1})}}>count++</button>
                 <Link to="/">Already Have an account?</Link>
             </div>
         );
